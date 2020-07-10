023a72a8c73cbee48d1806c91df780112ee81ae0
jugal09xx <jug091000@gmail.com>
2020-07-10 12:38:23 +0000

    add about page

--- app.js
+++ app.js
@@ -34,6 +34,10 @@ app.get("/news", function(req, res){
 	})
 });
 
+app.get("/about", function(req, res){
+	res.render("about");
+});
+
 app.listen(process.env.PORT || 3000, process.env.IP, function(){
 	console.log("server running...");
 });
